26e8345240d349bb7ddb663579f63992000782d0
Emdhi <34493524+Emdhi@users.noreply.github.com>
2017-12-13 09:12:50 +0800

    Add files via upload

---
+++ app/jobs.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class jobs extends Model
+{
+    //Table Name
+    protected $table = 'jobs';
+    
+    //Primary Key
+    public $primaryKey = 'job_id';
+}
