b3767046fe775fe63b91bafcea05252a9f722220
Emdhi <34493524+Emdhi@users.noreply.github.com>
2017-12-13 10:23:43 +0800

    Update home.blade.php

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -7,33 +7,40 @@
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <!-- Compiled and minified CSS -->
         <link rel="stylesheet" href="<?php echo asset('css/materialize.min.css')?>" type="text/css">
+        
         {{--  Custom CSS designed especially for Home Layout  --}}
         <link rel="stylesheet" href="<?php echo asset('css/customhome/home.css')?>" type="text/css">
+        
         {{--  Custom CSS for Loading  --}}
         <link rel="stylesheet" href="<?php echo asset('css/loader.css')?>" type="text/css"> 
+        
         {{--  FONT of the Website  --}}
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        
         {{--  Font Awesome for Icons  --}}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">      
+        
         <title>{{config('app.name','PBO Global')}}</title>
     </head>
     <body>
         <!--Import jQuery before materialize.js-->
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        
         <!--JavaScript at end of body for optimized loading-->
         <script type="text/javascript" src="js/materialize.min.js"></script>
         <script type="text/javascript" src="<?php echo asset('js/customhome.js')?>"></script>
         
+         {{--  API Key for Google Map :)  --}}
         <script async defer
             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDpFIMwuKFSuEr-HQYMvEUqOCzCJt9CCu8&callback=initMap">
-            // API Key for Google Map :)
         </script>
 
-        {{--  This is the loading animation  --}}
-        <div id="load">
-            <div class="dot1"></div>
-            <div class="dot2"></div>
-        </div>
+            {{--  Loading animation  --}}
+                <div id="load">
+                    <div class="dot1"></div>
+                    <div class="dot2"></div>
+                </div>
+            {{-- End of Loading animation  --}}
 
             <div id="contents">{{-- This div is for wrapping up the whole content for loading screen   --}}
                 <div id="fadeeffect" class="preload">
@@ -42,29 +49,29 @@
                         <div class="parallax">
                             <h3>Local Talent, Global Appeal</h3>
                             <img style="z-index: -1;" src=<?php echo asset("img/PBO.jpg")?>>
-                        
                         </div>
                     </div>
 
                     <div class="section white">                        
-                        @yield('content')
-                        @yield('MapArea')
-                        
+                        @yield('content') {{--   Content of Index  --}}
+                        @yield('MapArea') {{--  PboGlobal Map  --}}
                     </div>
+
                     <!-- Footer -->
-                    <footer class="page-footer">
-                        <div class="footer-copyright">
-                            <div class="container">
-                                <center>
-                                    <p>PBO Global © 2017 All Rights Reserved</p>
-                                    <a href="#" class="fa fa-facebook  tooltipped" data-position="top" data-delay="50" data-tooltip="Facebook"></a>
-                                    <a href="#" class="fa fa-twitter  tooltipped" data-position="top" data-delay="50" data-tooltip="Twitter"></a>
-                                    <a href="#" class="fa fa-linkedin  tooltipped" data-position="top" data-delay="50" data-tooltip="LinkedIn"></a>
-                                </center> 
-                            </div>
-                        </div>       
-                    </footer>
+                        <footer class="page-footer">
+                            <div class="footer-copyright">
+                                <div class="container">
+                                    <center>
+                                        <p>PBO Global © 2017 All Rights Reserved</p>
+                                        <a href="#" class="fa fa-facebook  tooltipped" data-position="top" data-delay="50" data-tooltip="Facebook"></a>
+                                        <a href="#" class="fa fa-twitter  tooltipped" data-position="top" data-delay="50" data-tooltip="Twitter"></a>
+                                        <a href="#" class="fa fa-linkedin  tooltipped" data-position="top" data-delay="50" data-tooltip="LinkedIn"></a>
+                                    </center> 
+                                </div>
+                            </div>       
+                        </footer>
+                    <!-- End of Footer -->
+                </div>
             </div>
-        </div>
     </body>
 </html>
